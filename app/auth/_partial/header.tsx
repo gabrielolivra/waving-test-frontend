5a4a70f4617ebcb5837b5b7a60d533de273df5ae
gabrielolivra <gabernardinooliveira@gmail.com>
2025-06-15 08:59:25 -0300

    adjust header app auth

--- app/auth/_partial/header.tsx
+++ app/auth/_partial/header.tsx
@@ -9,7 +9,7 @@ export default function Header({ session }: { session: Session | null }) {
 
   return (
     <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-16 bg-white shadow-md z-50">
-      <div></div>
+      <div><p className="font-bold ml-2">Waving test</p></div>
       <ProfileDropdown
         userName={session?.user?.name || 'Usuário'}
       />
--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,16 @@ export default function Page() {
   return (
     <main className="flex flex-col">
       <div className="flex items-center justify-between w-full p-0">
-        <div className="flex flex-col justify-center h-screen w-1/2 rounded-lg ml-8">
+        <div className="flex flex-col justify-center h-screen w-screen items-center rounded-lg ml-8">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Desafio waving test</strong><br></br>
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-waving-primary-light px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-waving-primary-light/50 md:text-base"
+            className="flex items-center gap-5 rounded-lg bg-waving-primary-light px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-waving-primary-light/50 md:text-base"
           >
             <span>Acessar e-comerce</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
-        </div>
-        <div className="w-1/2">
-          <Image
-            src="/favicon.ico"
-            width={600}
-            height={800}
-            className="w-screen h-screen"
-            quality={100}
-            alt=""
-          />
         </div>
       </div>
     </main >
